8f1b3e9e4cbb7330b97560f43a0bc80f1b1c0879
박영환 <52000618+rkdmfqka@users.noreply.github.com>
2021-08-02 13:12:53 +0900

    BOJ 1271/메모리 17876kb/시간 232ms

---
+++ 1271.java
@@ -0,0 +1,15 @@
+package first_semester;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger n = sc.nextBigInteger();
+		BigInteger m = sc.nextBigInteger();
+		System.out.println(n.divide(m));
+		System.out.println(n.remainder(m));
+		sc.close();
+	}
+}
