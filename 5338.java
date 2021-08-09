65a59971b8ea40f159fb70dc037ac35b1c65b8de
박영환 <52000618+rkdmfqka@users.noreply.github.com>
2021-08-09 11:29:17 +0900

    BOJ 5338/memory 13952kb/time 132ms

---
+++ 5338.java
@@ -0,0 +1,11 @@
+package first_semester;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._"); 
+		System.out.println("'-..-'|   ||   |"); 
+		System.out.println("'-..-'|_.-;;-._|"); 
+		System.out.println("'-..-'|   ||   |"); 
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
