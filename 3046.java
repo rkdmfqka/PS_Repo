5039ff5bd58d6c4a256f921ca6b2388a71198cbb
박영환 <52000618+rkdmfqka@users.noreply.github.com>
2021-08-09 11:09:57 +0900

    BOJ 3046/메모리 17520kb/시간 208ms

---
+++ 3046.java
@@ -0,0 +1,13 @@
+package first_semester;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r1 = sc.nextInt();
+		int s = sc.nextInt();
+		System.out.println((s * 2) - r1);
+		sc.close();
+	}
+}
