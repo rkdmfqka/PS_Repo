6fab86165f6b2e9f7ae4960bdc89d8dd1ab73c35
박영환 <52000618+rkdmfqka@users.noreply.github.com>
2021-08-13 17:21:16 +0900

    BOJ 14652/memory 18380kb/time 232ms

---
+++ 14652.java
@@ -0,0 +1,14 @@
+package first_semester;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int k=sc.nextInt();
+		System.out.println(k/m + " " + k%m);
+		sc.close();
+	}
+}
