430c655900883d52272340fa169f39f13364a7e3
rkdmfqka <pch12900@naver.com>
2022-02-18 11:57:32 +0900

    BOJ 2752/memory 18404kb/time 244ms

---
+++ 2752.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] num=new int[3];
+        for(int i=0; i < 3; i++)num[i]=sc.nextInt();
+        Arrays.sort(num);
+        for(int i=0; i<3; i++)System.out.print(num[i] + " ");
+        sc.close();
+    }
+}
