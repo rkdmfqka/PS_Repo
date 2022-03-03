c2f9e08683f2e85c6b1504035ba4e3088a11dc7b
rkdmfqka <pch12900@naver.com>
2022-03-03 20:51:24 +0900

    BOJ 5596/memory 17660kb/time 216ms

---
+++ 5596.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0, b = 0;
+        for(int i=0; i<4; i++) a += sc.nextInt();
+        for(int i=0; i<4; i++) b += sc.nextInt();
+        System.out.println(Math.max(a, b));
+        sc.close();
+    }
+}
