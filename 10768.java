ca147faef262a1e9d69fea44ae3dd060a9b42e37
rkdmfqka <pch12900@naver.com>
2022-03-07 21:35:44 +0900

    BOJ 10768/memory 17544kb/time 208ms

---
+++ 10768.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        if(month<2) System.out.println("Before");
+        else if(month==2){
+            if(day<18) System.out.println("Before");
+            else if(day==18) System.out.println("Special");
+            else System.out.println("After");
+        }
+        else System.out.println("After");
+        sc.close();
+    }
+}
