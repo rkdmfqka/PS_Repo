c322725c7e79c38193f9b8b6321e3fccfec6f2f1
rkdmfqka <pch12900@naver.com>
2022-03-18 17:17:21 +0900

    BOJ 2576/memory 17660kb/time 208ms

---
+++ 2576.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int min = 100;
+        for(int i=0; i<7; i++){
+            int n = sc.nextInt();
+            if(n%2==1){
+                sum+=n;
+                min=Math.min(min, n);
+            }
+        }
+        if(sum!=0){
+            System.out.println(sum);
+            System.out.println(min);
+        }
+        else System.out.println(-1);
+        sc.close();
+    }
+}
