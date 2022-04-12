fc76e6c78fb7fcedadea5185e23c4f18f90eb1b5
rkdmfqka <pch12900@naver.com>
2022-04-12 23:16:32 +0900

    BOJ 10250/memory 18208kb/time 248ms

---
+++ 10250.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i=0; i<t; i++){
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int n = sc.nextInt();
+            if (n % h == 0) System.out.println((h * 100) + (n / h));
+            else System.out.println(((n % h) * 100) + (n / h) + 1);
+        }
+        sc.close();
+    }
+}
