b2566efe78c2232256eaf7ba544c08433ac0740a
rkdmfqka <pch12900@naver.com>
2022-04-17 23:12:59 +0900

    BOJ 5717/memory 17664kb/time 204ms

---
+++ 5717.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            int m = sc.nextInt();
+            int f = sc.nextInt();
+            if (m==0 && f==0) break;
+            System.out.println(m+f);
+        }
+        sc.close();
+    }
+}
