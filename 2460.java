68ae680e5ece004f7cc38e737100a82294bc6169
rkdmfqka <pch12900@naver.com>
2022-05-04 22:31:33 +0900

    BOJ 2460/memory 17720kb/time 204ms

---
+++ 2460.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+        int sum = 0;
+        for (int i=0; i<10; i++){
+            int out = sc.nextInt();
+            int in = sc.nextInt();
+            sum -= out;
+            sum += in;
+            max = Math.max(max, sum);
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
