fd421eb4800a0b4b39ee4f10a03162c8602b2309
rkdmfqka <pch12900@naver.com>
2022-05-05 22:46:30 +0900

    BOJ 2506/memory 17724kb/time 220ms

---
+++ 2506.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) arr[i] = sc.nextInt();
+        int cnt = 0;
+        int sum = 0;
+        for (int i=0; i<n; i++){
+            if (arr[i]==1){
+                cnt++;
+                sum+=cnt;
+            }
+            else cnt = 0;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
