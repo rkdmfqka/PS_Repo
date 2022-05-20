a51a33ffd63c9eb7094d71b8e4b56ef832c17620
rkdmfqka <pch12900@naver.com>
2022-05-20 23:26:33 +0900

    BOJ 3052/memory 17692kb/time 204ms

---
+++ 3052.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i=0; i<10; i++) hs.add(sc.nextInt()%42);
+        System.out.println(hs.size());
+        sc.close();
+    }
+}
